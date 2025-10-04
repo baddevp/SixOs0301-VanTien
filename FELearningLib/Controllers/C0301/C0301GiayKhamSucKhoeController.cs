916307dc0fbdf9ecb85c4bc6e3f70ddc5064b348
baddevp <20114581.tien@student.iuh.edu.vn>
2025-10-04 16:13:09 +0700

    update

---
+++ FELearningLib/Controllers/C0301/C0301GiayKhamSucKhoeController.cs
@@ -0,0 +1,28 @@
+﻿using FELearningLib.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace FELearningLib.Controllers.C0301
+{
+    [Route("GiayKhamSucKhoe")]
+    public class C0301GiayKhamSucKhoeController : Controller
+    {
+        //private readonly S0301StoreProceduceService _spService;
+        //private readonly S0301DynamicClassGeneratorService _classGenerator;
+        //private readonly IWebHostEnvironment _env;
+
+        //public C0301GiayKhamSucKhoeController(S0301StoreProceduceService spService, S0301DynamicClassGeneratorService classGenerator, IWebHostEnvironment env)
+        //{
+        //    _spService = spService;
+        //    _classGenerator = classGenerator;
+        //    _env = env;
+        //}
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("~/Views/V0301GiayKhamSucKhoeLaiXe/Index.cshtml");
+        }
+    }
+}
+
