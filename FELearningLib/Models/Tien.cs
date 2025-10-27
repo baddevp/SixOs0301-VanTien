07d9d68f1b53119ad507a19faacaf8b7849fb77c
baddevp <20114581.tien@student.iuh.edu.vn>
2025-10-27 17:55:51 +0700

    change 2710:5555

---
+++ FELearningLib/Models/Tien.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace FELearningLib.Models
+{
+    public class Tien
+    {
+        public string A { get; set; }
+        public string B { get; set; }
+        public string C { get; set; }
+        public string D { get; set; }
+    }
+}
